66b0a77898d5e731019831f649f70dc3f5b2953a
Denis <denisarenko@gmail.com>
2025-07-15 18:00:36 +0300

    Add ChartBar Component

--- components/app/chart/Bar.vue
+++ components/app/chart/Bar.vue
@@ -1,7 +1,5 @@
 <template>
-  <VisXYContainer :data="chartData.data" height="25rem" width="100%">
-    <AppChartLegend v-model="chartData" :categories :colors :data class="mb-4" />
-
+  <VisXYContainer :data="chartData" height="25rem" width="100%">
     <VisBar
       :x="(d: T, i: number) => i"
       :y="categories.map((category) => (d: T) => d[category])"
@@ -14,13 +12,18 @@
       type="x"
       :domain-line="false"
       :grid-line="false"
-      :tick-format="(i: number) => (xFormat ? xFormat(data[i]![xKey]) : data[i]![xKey])"
+      :tick-format
       :num-ticks="data.length"
+      :tick-text-fit-mode="xTrim ? 'trim' : 'none'"
+      :tick-text-color="(i: number) => (xIndexes.has(i) ? 'hsl(from var(--color-surface) h s l / 40%)' : '')"
+      :events="{
+        [Axis.selectors.tick]: { click: (i: number) => toggleX(i) }
+      }"
     />
 
     <VisAxis type="y" :tick-format="(i: number) => (yFormat ? yFormat(i) : formatNumber(i))" />
 
-    <AppChartCrosshair :categories="chartData.categories || categories" :colors :x-key :y-format :x-format />
+    <AppChartCrosshair :categories :colors :x-key :y-format :x-format />
 
     <svg width="0" height="0">
       <linearGradient v-for="(color, i) in colors" :id="`vis-bar-g-${i}`" :key="i" x1="0" y1="0" x2="0" y2="1">
@@ -31,14 +34,16 @@
   </VisXYContainer>
 </template>
 
-<script setup lang="ts" generic="T extends Record<string, unknown | Record<string, unknown>>">
+<script setup lang="ts" generic="T extends Record<string, unknown>">
 import { VisXYContainer, VisAxis, VisGroupedBar, VisStackedBar } from '@unovis/vue';
+import { Axis } from '@unovis/ts';
 
 const props = defineProps<{
   data: T[];
-  categories: Extract<keyof T, string>[];
+  categories: string[];
   xKey: Extract<keyof T, string>;
-  xFormat?: (i: T[string] | number) => string;
+  xTrim?: boolean;
+  xFormat?: (i: string | number) => string | Date;
   yFormat?: (i: string | number) => string;
   stacked?: boolean;
   roundedCorners?: number;
@@ -46,7 +51,36 @@ const props = defineProps<{
 
 const VisBar = computed(() => (props.stacked ? VisStackedBar : VisGroupedBar));
 
-const chartData = ref<T[]>([]);
+const originalData = structuredClone(props.data);
+const chartData = ref<T[]>(props.data);
+
+const xIndexes = ref(new Set<number>());
+
+const toggleX = (index: number) => {
+  const isHidden = xIndexes.value.has(index);
+  const original = originalData[index];
+  const toggled = chartData.value[index] as Record<string, unknown>;
+
+  if (!original || !toggled) return;
+
+  for (const category of props.categories) {
+    if (!original[category]) continue;
+
+    if (isHidden) {
+      xIndexes.value.delete(index);
+      toggled[category] = original[category];
+    } else {
+      xIndexes.value.add(index);
+      toggled[category] = 0;
+    }
+  }
+};
+
+const tickFormat = (i: number) => {
+  const value = props.data[i]?.[props.xKey] as string | number;
+
+  return props.xFormat ? props.xFormat(value) : value;
+};
 
 const colors = [
   'var(--color-accent)',
