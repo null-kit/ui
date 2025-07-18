dfd27e4d462689015b2443b3b45f261783a10fca
Denis <denisarenko@gmail.com>
2025-07-18 20:24:04 +0300

    Update Modal Component

--- components/app/Modal.vue
+++ components/app/Modal.vue
@@ -1,39 +1,38 @@
 <template>
-  <Teleport to="#teleports">
+  <Teleport to="#teleports" class="perspective-distant">
+    <Transition enter-from-class="opacity-0" enter-to-class="opacity-100" leave-to-class="opacity-0">
+      <div v-if="isActive" class="fixed inset-0 z-10 bg-black/50 backdrop-blur-xs duration-400" />
+    </Transition>
+
     <Transition
-      enter-from-class="opacity-0"
-      enter-to-class="opacity-100 duration-300"
-      leave-to-class="opacity-0 duration-300"
+      enter-from-class="opacity-0 modal-motion"
+      enter-to-class="duration-400"
+      leave-to-class="opacity-0 modal-motion duration-400"
       @after-enter="(el) => (el as HTMLDivElement).focus()"
-      @after-leave="isActive = false"
     >
       <div
         v-if="isActive"
-        class="fixed inset-0 z-10 flex overflow-auto bg-black/50 p-4 outline-0 backdrop-blur-xs md:p-10"
+        role="dialog"
+        class="fixed inset-0 z-10 flex overflow-auto p-4 outline-0 md:p-10"
         tabindex="0"
         @keydown.esc="onClose"
+        @click.self="onClose"
       >
-        <Transition
-          appear-from-class="scale-98 translate-y-5"
-          appear-to-class="scale-100 translate-y-0 duration-300"
-          appear
-        >
-          <div class="modal-content m-auto w-full" :class="modalClass">
-            <slot v-if="$slots.header || $slots.title" name="header">
-              <div class="flex items-center p-4">
-                <slot v-if="$slots.title" name="title" />
+        <div class="modal-content m-auto w-full" :class="modalClass">
+          <slot v-if="$slots.header || $slots.title" name="header">
+            <div class="flex items-center p-4">
+              <slot v-if="$slots.title" name="title" />
 
-                <button type="button" class="ml-auto opacity-50 duration-200 hover:opacity-100" @click="onClose">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="size-6">
-                    <path d="M25 7L7 25 M25 25L7 7" stroke="currentColor" stroke-width="2" />
-                  </svg>
-                </button>
-              </div>
-            </slot>
+              <button type="button" class="ml-auto opacity-50 duration-200 hover:opacity-100" @click="onClose">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="size-6">
+                  <path d="M25 7L7 25 M25 25L7 7" stroke="currentColor" stroke-width="2" />
+                </svg>
+              </button>
+            </div>
+          </slot>
 
-            <slot />
-          </div>
-        </Transition>
+          <slot />
+        </div>
       </div>
     </Transition>
   </Teleport>
