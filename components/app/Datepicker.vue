30052f15d39a15f50088aee86cbdc0346c73c100
Denis <denisarenko@gmail.com>
2025-08-07 14:08:20 +0300

    Update Datepicker Component

--- components/app/Datepicker.vue
+++ components/app/Datepicker.vue
@@ -261,7 +261,7 @@ const setPreset = (type: Preset) => {
       break;
     case 'yesterday':
       selectDate(new Date(today.setDate(today.getDate() - 1)));
-      if (props.range) selectDate(new Date(today.setDate(today.getDate() - 1)));
+      if (props.range) selectDate(new Date(today.setDate(today.getDate())));
       break;
     case 'last-week':
       selectDate(new Date(today.setDate(today.getDate() - 7)));
