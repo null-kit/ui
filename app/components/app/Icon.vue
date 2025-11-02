b78eee4e5ea3906a2fc64e16552687ea0bbb010a
Denis <denisarenko@gmail.com>
2025-11-02 20:56:52 +0200

    Refactor Components

--- app/components/app/Icon.vue
+++ app/components/app/Icon.vue
@@ -6,6 +6,14 @@
   </svg>
 </template>
 
+<script lang="ts">
+const icons = import.meta.glob<string>('@/assets/img/svg/*.svg', {
+  query: '?raw',
+  import: 'default',
+  eager: false
+});
+</script>
+
 <script setup lang="ts">
 const props = defineProps<{ name: string; inline?: boolean }>();
 
@@ -17,12 +25,23 @@ const iconInline = shallowRef();
 
 onMounted(async () => {
   if (props.inline) {
-    const iconRaw = await import(`@/app/assets/img/svg/${props.name}.svg?raw`);
+    const iconRaw = Object.entries(icons).find(([path]) => path.endsWith(`/${props.name}.svg`));
 
-    iconInline.value = iconRaw.default
-      .replace(/\n/g, '')
-      .replace(/<svg/, '<svg width="100%" height="100%" fill="none"')
-      .replace(/fill="(?!none")[^"]*"/g, 'fill="currentColor"');
+    if (!iconRaw) {
+      console.error(`Icon '${props.name}' not found`);
+      return;
+    }
+
+    try {
+      const raw = await iconRaw[1]();
+
+      iconInline.value = raw
+        .replace(/\n/g, '')
+        .replace(/<svg/, '<svg width="100%" height="100%"')
+        .replace(/fill="(?!none")[^"]*"/g, 'fill="currentColor"');
+    } catch (error) {
+      console.error(`Failed to load icon '${props.name}'`, error);
+    }
   }
 });
 </script>
