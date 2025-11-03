3f821e1985259d03667bcc0999b5486e744a703a
Denis <denisarenko@gmail.com>
2025-11-03 17:02:48 +0200

    Refactor Components

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'node:url';
 const rootDir = fileURLToPath(new URL('..', import.meta.url));
 
 export default defineNuxtConfig({
+  compatibilityDate: '2025-11-03',
   ssr: false,
   extends: ['..'],
   modules: ['@nuxt/eslint'],
