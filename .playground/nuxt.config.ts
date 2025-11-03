6416dc185df3cb538b312e96ffc90e4f643b57f9
Denis <denisarenko@gmail.com>
2025-11-03 13:37:06 +0200

    Update Deploy Path

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -1,23 +1,18 @@
 import { fileURLToPath } from 'node:url';
-import { join } from 'node:path';
-
-const playgroundDir = fileURLToPath(new URL('.', import.meta.url));
-const rootDir = fileURLToPath(new URL('..', import.meta.url));
 
 export default defineNuxtConfig({
   ssr: false,
   extends: ['..'],
-  alias: {
-    '@null-kit/ui': join(rootDir, 'app', 'assets', 'css')
-  },
-  srcDir: join(playgroundDir, 'app'),
   modules: ['@nuxt/eslint'],
   eslint: {
     config: {
       rootDir: fileURLToPath(new URL('..', import.meta.url))
     }
   },
-  css: [join(playgroundDir, 'app', 'assets', 'css', 'app.css')],
+  alias: {
+    '@null-kit/ui': fileURLToPath(new URL('../app/assets/css', import.meta.url))
+  },
+  css: ['@/assets/css/app.css'],
   postcss: {
     plugins: {
       '@tailwindcss/postcss': {}
