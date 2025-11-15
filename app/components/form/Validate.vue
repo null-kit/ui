fee7f8176682ef7728fabc6818798957238d3cd1
Denis <denisarenko@gmail.com>
2025-11-15 13:46:42 +0200

    Update FormValidate

--- app/components/form/Validate.vue
+++ app/components/form/Validate.vue
@@ -1,9 +1,6 @@
 <template>
   <Transition enter-from-class="translate-y-2 blur-xs skew-4 opacity-0" enter-to-class="duration-300" appear>
-    <div
-      v-if="name && validate[name]"
-      class="bg-surface text-newton pointer-events-none absolute top-full left-3 -mt-2 rounded-md px-2 py-0.5 text-xs font-medium shadow"
-    >
+    <div v-if="name && validate[name]" class="form-validate">
       {{ validate[name] }}
     </div>
   </Transition>
