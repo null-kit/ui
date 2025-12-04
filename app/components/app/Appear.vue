6a9dfda530ec47ab60ec795f8f59131ed8fa0889
Denis <denisarenko@gmail.com>
2025-12-05 00:46:42 +0200

    Update AppAppear

--- app/components/app/Appear.vue
+++ app/components/app/Appear.vue
@@ -1,5 +1,13 @@
 <template>
-  <Transition appear-from-class="opacity-0 translate-y-2 skew-2" appear-to-class="origin-top-left duration-300" appear>
+  <Transition
+    appear-from-class="opacity-0 translate-y-2 skew-2"
+    appear-to-class="origin-top-left duration-300"
+    :appear="!noAppear"
+  >
     <slot />
   </Transition>
 </template>
+
+<script setup lang="ts">
+defineProps<{ noAppear?: boolean }>();
+</script>
