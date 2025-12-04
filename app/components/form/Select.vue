03dbe51c8c0cbcdb84ee068a567db5681ac311c6
Denis <denisarenko@gmail.com>
2025-12-04 22:53:31 +0200

    Update FormSelect

--- app/components/form/Select.vue
+++ app/components/form/Select.vue
@@ -188,7 +188,7 @@ const props = defineProps<{
   help?: string;
   presets?: {
     name: string;
-    list: string[];
+    list: string[] | number[];
   }[];
   inputClass?: string;
   validateClass?: string;
