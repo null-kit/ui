7b19a5367e7d8ece3a981f2325a5440ae03a8973
Denis <denisarenko@gmail.com>
2026-04-27 18:00:54 +0300

    Update AppBadge

--- app/components/app/Badge.vue
+++ app/components/app/Badge.vue
@@ -1,8 +1,12 @@
 <template>
   <span
     class="inline-flex items-center justify-center gap-1.5 font-medium ring ring-inset"
-    :class="[type && styles[type], size && sizes[size]]"
+    :class="{
+      [`badge-${type}`]: type,
+      [`badge-${size}`]: size
+    }"
     :aria-label="`badge-${type}`"
+    role="badge"
   >
     <slot>
       <slot name="icon">
@@ -30,19 +34,4 @@ const {
   icon?: string;
   size?: 'default' | 'sm' | 'lg';
 }>();
-
-const styles = {
-  default: 'bg-darwin ring-edison shadow-xs text-surface dark:shadow-black/20',
-  success: 'bg-green-500/10 ring-green-500/20 text-green-600',
-  warning: 'bg-yellow-500/10 ring-yellow-500/20 text-yellow-600',
-  danger: 'bg-red-500/10 ring-red-500/20 text-red-500',
-  info: 'bg-blue-500/10 ring-blue-500/20 text-blue-500',
-  accent: 'bg-accent/10 ring-accent/20 text-accent'
-};
-
-const sizes = {
-  default: 'px-2 py-1 text-xs rounded-md',
-  sm: 'px-1.5 py-0.5 text-xs rounded-md',
-  lg: 'px-2.5 py-1.5 text-sm rounded-lg'
-};
 </script>
