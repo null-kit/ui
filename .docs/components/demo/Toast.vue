20b21e385ccf26b008da29141c4f2e9396fa1569
Denis <denisarenko@gmail.com>
2026-04-28 18:58:10 +0300

    Minor Update

---
+++ .docs/components/demo/Toast.vue
@@ -0,0 +1,51 @@
+<template>
+  <fieldset id="toasts">
+    <legend>Toasts</legend>
+
+    <div class="flex gap-4">
+      <button
+        type="button"
+        class="btn btn-default"
+        @click="setToast({ title: 'Nice!', text: 'This is a default toast!', type: 'default' })"
+      >
+        Default Toast
+      </button>
+
+      <button
+        type="button"
+        class="btn btn-default"
+        @click="
+          setToast({
+            title: 'Success!',
+            text: 'Your operation was successful!',
+            type: 'success'
+          })
+        "
+      >
+        Success Toast
+      </button>
+
+      <button
+        type="button"
+        class="btn btn-default"
+        @click="setToast({ title: 'Error!', text: 'Your operation was failed!', type: 'error' })"
+      >
+        Error Toast
+      </button>
+
+      <button
+        type="button"
+        class="btn btn-default"
+        @click="setToast({ title: 'Info!', text: 'Your operation was okay!', type: 'info' })"
+      >
+        Info Toast
+      </button>
+    </div>
+  </fieldset>
+</template>
+
+<script setup lang="ts">
+import { useToast } from '../../../app/composables/use-toast';
+
+const { setToast } = useToast();
+</script>
--- .docs/pages/kitchen-sink.vue
+++ .docs/pages/kitchen-sink.vue
@@ -146,49 +146,7 @@
           </div>
         </fieldset>
 
-        <fieldset id="toasts">
-          <legend>Toasts</legend>
-
-          <div class="flex gap-4">
-            <button
-              type="button"
-              class="btn btn-default"
-              @click="setToast({ title: 'Nice!', text: 'This is a default toast!' })"
-            >
-              Default Toast
-            </button>
-
-            <button
-              type="button"
-              class="btn btn-default"
-              @click="
-                setToast({
-                  title: 'Success!',
-                  text: 'Your operation was successful!',
-                  type: 'success'
-                })
-              "
-            >
-              Success Toast
-            </button>
-
-            <button
-              type="button"
-              class="btn btn-default"
-              @click="setToast({ title: 'Error!', text: 'Your operation was failed!', type: 'error' })"
-            >
-              Error Toast
-            </button>
-
-            <button
-              type="button"
-              class="btn btn-default"
-              @click="setToast({ title: 'Info!', text: 'Your operation was okay!', type: 'info' })"
-            >
-              Info Toast
-            </button>
-          </div>
-        </fieldset>
+        <DemoToast />
 
         <DemoChart />
 
@@ -316,9 +274,12 @@
 </template>
 
 <script setup lang="ts">
-import { useToast } from '../../app/composables/use-toast';
+useHead({
+  bodyAttrs: {
+    class: 'bg-darwin'
+  }
+});
 
-const { setToast } = useToast();
 const { copy, statusText } = useClipboard();
 
 const formData = reactive({
