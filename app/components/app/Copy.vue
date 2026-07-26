d69d4bb6051496e3e214d4f7a30cfdf0d94f49b5
Denis <denisarenko@gmail.com>
2026-07-26 17:30:01 +0300

    Update AppCopy

--- app/components/app/Copy.vue
+++ app/components/app/Copy.vue
@@ -1,19 +1,21 @@
 <template>
-  <button type="button" class="btn" title="Copy" @click="copy(value)">
+  <button type="button" class="btn" title="Copy" @click.stop="copy(value)">
     <Transition
       enter-from-class="scale-80 blur-xs opacity-0"
       enter-to-class="duration-200"
       leave-to-class="scale-80 blur-xs opacity-0"
       mode="out-in"
     >
-      <AppIcon v-if="status" name="copied" class="text-accent" />
-      <AppIcon v-else name="copy" />
+      <AppIcon v-if="status" name="copied" class="text-accent" :class="iconClass" />
+      <AppIcon v-else name="copy" :class="iconClass" />
     </Transition>
+
+    <slot>{{ label }}</slot>
   </button>
 </template>
 
 <script setup lang="ts">
 const { copy, status } = useClipboard();
 
-defineProps<{ value: string }>();
+defineProps<{ value: string; iconClass?: string; label?: string }>();
 </script>
